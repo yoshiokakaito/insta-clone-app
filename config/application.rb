0f52e99500d07250fd80ed40794cf59a016d6d2d
EC2 Default User <ec2-user@ip-172-31-43-60.ap-northeast-1.compute.internal>
2020-04-09 04:08:48 +0000

    rspecの環境構築

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,13 @@ module InstaApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    
+    config.generators do |g|
+      g.test_framework :rspec,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
