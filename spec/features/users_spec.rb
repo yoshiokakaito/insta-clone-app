f5261e54ed504334515e6f553ca69ccf38a906c7
EC2 Default User <ec2-user@ip-172-31-43-60.ap-northeast-1.compute.internal>
2020-04-11 07:04:14 +0000

    プロフィールページの投稿からコメント見れるように修正、ユーザー自身でアカウント削除できる機能を追加

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -6,19 +6,19 @@ RSpec.feature "Users", type: :feature do
     visit signup_path
     
     expect {
-      fill_in "User name", with: user.user_name
-      fill_in "Name", with: user.name
-      fill_in "Email", with: user.email
-      fill_in "Password", with: user.password_digest
-      fill_in "Password confirmation", with: user.password_digest
-      click_button "Create my account"
+      fill_in "ユーザーネーム", with: user.user_name
+      fill_in "名前", with: user.name
+      fill_in "メールアドレス", with: user.email
+      fill_in "パスワード", with: user.password_digest
+      fill_in "確認", with: user.password_digest
+      click_button "登録"
     }.to change(User, :count).to(1)
   end
   
   scenario "エラーメッセージが正しく表示されるか" do
     visit signup_path
    
-    click_button "Create my account"
+    click_button "登録"
     expect(page).to have_content("can't be blank")
   end
   
